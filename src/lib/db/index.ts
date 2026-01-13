f65562923c9e1fbf759f41bef53f4bac8ddbbba1
vaehalili <vaehalili@gmail.com>
2026-01-13 22:12:32 +0800

    feat: [db-1] - Local database schema

---
+++ src/lib/db/index.ts
@@ -0,0 +1,25 @@
+import Dexie, { type EntityTable } from 'dexie';
+import type { Exercise, Entry, Session, ParseQueueItem } from './types';
+
+export class FinishStrongDB extends Dexie {
+  exercises!: EntityTable<Exercise, 'id'>;
+  entries!: EntityTable<Entry, 'id'>;
+  sessions!: EntityTable<Session, 'id'>;
+  parseQueue!: EntityTable<ParseQueueItem, 'id'>;
+
+  constructor() {
+    super('FinishStrongDB');
+
+    this.version(1).stores({
+      exercises: 'id, name, updatedAt',
+      entries: 'id, exerciseId, sessionId, createdAt, updatedAt, synced',
+      sessions: 'id, date, startedAt, endedAt, updatedAt, synced',
+      parseQueue: 'id, status, createdAt'
+    });
+  }
+}
+
+export const db = new FinishStrongDB();
+
+export * from './types';
+export { generateId } from './utils';
